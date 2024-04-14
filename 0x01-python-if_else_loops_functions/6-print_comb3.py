40d77de7bf9b08b323297b64ae25aedbef8e24fc
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-14 04:43:48 -0700

    All possible combinations of two digit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+x = 0
+for i in range(10):
+    for n in range(x, 10):
+        if i == n:
+            continue
+        if i < 8:
+            print("{}{}, ".format(i, n), end='')
+        else:
+            print("{}{}".format(i, n), end='')
+    x += 1
+print("\n")

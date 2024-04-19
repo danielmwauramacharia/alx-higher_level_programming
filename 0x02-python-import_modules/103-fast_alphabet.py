8e537065ed8d5207eec61c645d52469c4fdc25b1
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-19 14:24:00 -0700

    A function that prints alphabets in upper case using  import modules

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from aphabets import alpha_upper
+print(alpha_upper())
---
+++ 0x02-python-import_modules/aphabets.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def alpha_upper():
+    str1 = ""
+    for i in range(65, 91):
+        str1 += chr(i)
+    return str1

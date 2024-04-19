954d6743ef6cba2f0b9090df310af8076d1315aa
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-19 04:00:17 -0700

    bug fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 import sys
-length = len(sys.argv)
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-if length != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-if sys.argv[2] == '+':
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    sys.exit(0)
-elif sys.argv[2] == '-':
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    sys.exit(0)
-elif sys.argv[2] == '*':
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    sys.exit(0)
-elif sys.argv[2] == '/':
-    print("{} / {} = {}".format(a, b, div(a, b)))
-    sys.exit(0)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+if __name__ == "__main__":
+    length = len(sys.argv)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if length != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if sys.argv[2] == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+        sys.exit(0)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)

9678faf20c03641277981537116591d12d2408d3
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-22 03:09:24 -0700

    adding tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) > len(tuple_b):
+        result = list(tuple_a)
+        shorter = tuple_b
+    else:
+        result = list(tuple_b)
+        shorter = tuple_a
+    for x, value in enumerate(shorter):
+        result[x] += value
+    return tuple(result)

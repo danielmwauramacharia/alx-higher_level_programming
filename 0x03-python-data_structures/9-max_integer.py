c7b3e1f2b6f4a953cb6b8dc610abf6d86dbcd4bc
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-22 05:27:32 -0700

    return the max number in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+    if len(my_list) == 1:
+        return my_list[0]
+    num = sorted(my_list)
+    return num[-1]

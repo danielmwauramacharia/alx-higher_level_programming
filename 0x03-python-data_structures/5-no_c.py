b8a1a376123816f0b35d12ab5c0f48df68a372bf
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-22 00:59:51 -0700

    fix the pepcodestyle

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     my_list = list(my_string)
-    filtered = [l for l in my_list if l.lower() != 'c']
+    filtered = [letter for letter in my_list if letter.lower() != 'c']
     my_string = ''.join(filtered)
     return my_string

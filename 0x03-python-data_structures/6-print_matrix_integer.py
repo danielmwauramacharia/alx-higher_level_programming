faf38d7f0f71b6340e0e1b6c2faaaa96e96604fe
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-04-30 06:13:25 -0700

    a new way of writing the code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 1 and len(matrix[0]) == 0:
-        print("{}".format(matrix))
-    for i in range(len(matrix)):
-        new_list = [num for num in matrix[i]]
-        for x, n in enumerate(new_list):
-            if x != (len(new_list) - 1):
-                print("{:d} ".format(n), end='')
-            else:
-                print("{:d}".format(n))
-        del new_list
+    if not matrix:
+        return
+    for item in matrix:
+        for x in item:
+            print("{:d} ".format(x), end='')
+        print()

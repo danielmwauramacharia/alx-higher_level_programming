aa72b21dc7e8e89dd25064bcdf3a4c7db692f7df
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-05-01 08:38:08 -0700

    remove the space after : in the message variable

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as ex:
-        message = "Exception: "
-        sys.stderr.write("{}{}".format(message, ex))
+        message = "Exception:"
+        sys.stderr.write("{0}{1}".format(message, ex))
         return False

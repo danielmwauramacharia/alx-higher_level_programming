a78486df7fcf023b187162ebe8de71c3578c0558
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-05-01 21:03:25 -0700

    Fix the arithmetics

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception("Too far")
-            result = (a ** b) / i
+                raise Exception('Too far')
+            result += (a ** b) / float(i)
         except Exception:
             pass
-        result = a + b
-        return result
+            result = a + b
+    return result

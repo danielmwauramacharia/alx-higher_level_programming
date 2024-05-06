8f2106e483349d7891aa4d33df0acffad9369eae
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-05-06 08:31:55 -0700

    Handling the exceptions:

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,13 +6,10 @@ class Square:
     """Defining properties of a Square
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialization of Instance attributes """
-        try:
-            if size is not int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        except (TypeError, ValueError):
-            pass
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size1 = size

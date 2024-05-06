a4e89ead1dc7bbe997c7cfafc490c4fdd362b7ab
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-05-06 07:17:03 -0700

    Initializing a private instance attribute in a class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+"""Defining a Square"""
+
+
+class Square:
+    """Defining properties of a Square
+    """
+
+    def __init__(self, size):
+        """Initialization of Instance attributes """
+        self.__size = size

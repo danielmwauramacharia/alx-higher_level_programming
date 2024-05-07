54a8e202943f1abcfab26cf58d79182f875e4a1c
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-05-06 17:35:14 -0700

    Use of property and setter and OOP

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defining a Square"""
+
+
+class Square:
+    """Defining properties of a Square
+    """
+
+    def __init__(self, size=0):
+        """Initialization of Instance attributes """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Return the controlled size"""
+        return self.__size
+
+    @size.getter
+    def size(self, value):
+        """The controls for the size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the Square"""
+        return (self.__size**2)

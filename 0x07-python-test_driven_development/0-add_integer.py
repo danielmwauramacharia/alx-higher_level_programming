bd154e2d08de835f258ea32b886250856985a6ee
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-05-14 04:29:11 -0700

    updating the function to nadle NaN value

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,10 @@
-#!/usr/bin/python3
 """Sum of two numbers"""
 
 
 def add_integer(a, b=98):
     """A function that returns sum of two numbers"""
-    if not isinstance(a, (int, float)):
+    if a != a or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (float, int)):
+    if b != b or not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+    return int(a) + int(b)

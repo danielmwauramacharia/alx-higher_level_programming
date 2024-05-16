407c2fbc0e8fed312040c7599576216853dc5707
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-05-16 05:20:33 -0700

    Adding the function documentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,6 +3,8 @@
 
 
 def text_indentation(text=""):
+    """function that prints a text with 2 new lines after
+    each of these characters: ., ? and :"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     words = text.split()

04a3b8303dea6458211ca04bffc92414f500a313
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-05 01:46:17 -0700

    Bug Fix

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -6,7 +6,7 @@ def matrix_mul(m_a, m_b):
     """A function that multiplies two matrices"""
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    if not isinstance(m_a, list):
+    if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
     if not all(isinstance(item, list) for item in m_a):
         raise TypeError("m_a must be a list of lists")

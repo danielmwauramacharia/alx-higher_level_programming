91ad52c9d9749e0bbc7dede1d80f0c9c6c8622b5
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-10 06:18:37 -0700

    a locked class, no additional attributes

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+"""No creating additional attributes"""
+
+
+class LockedClass:
+    """A class that have controlled attributes"""
+    __slots__ = ['first_name']
+
+    def __init__(self, first_name=None):
+        """Instantiation"""
+        self.first_name = first_name

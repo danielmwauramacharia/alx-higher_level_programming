5ab3e3a8c9be82007576d1b7f08f890a4f1c69e1
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-17 08:50:24 -0700

    which documentation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
-"""rectangle documentation"""
+"""
+Module that defines class rectangle based on class base geometry
+
+Its part of geometry package
+"""
 
 
 class Rectangle(BaseGeometry):
-    """Geometry Rectangle"""
+    """
+    class represent rectangle
+
+    inherits the attributes and methods from class basegeometry
+    """
 
     def __init__(self, width, height):
-        """Instantiation"""
+        """
+        Instantiation of Rectangle with specified width and height
+
+        args
+
+        width(int): width of the rectangle
+        height(int): height of the rectangle     
+        """
         if self.integer_validator("width", width) is True:
             self.__width = width
         if self.integer_validator("height", height) is True:

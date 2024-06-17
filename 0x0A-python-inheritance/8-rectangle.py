7d67b02a7d1add651bd7bcd3fba050e31c513fb7
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-17 08:28:26 -0700

    defining a class rectangle , inherits class BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defining a rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """Geometry: Rectangle"""
+
+    def __init__(self, width, height):
+        """Instantiation"""
+        if self.integer_validator("width", width) is True:
+            self.__width = width
+        if self.integer_validator("height", height) is True:
+            self.__height = height

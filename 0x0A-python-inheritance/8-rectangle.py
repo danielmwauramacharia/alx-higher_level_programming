c707923a0a5e0d35d8fd9577b9d6e780686bf5b7
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-17 09:08:14 -0700

    changes in the module

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
-"""
-Module that defines class rectangle based on class base geometry
 
-Its part of geometry package
-"""
+"""module defining the class rectangle"""
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    class represent rectangle
-
-    inherits the attributes and methods from class basegeometry
-    """
+    """defining class rectangle """
 
     def __init__(self, width, height):
-        """
-        Instantiation of Rectangle with specified width and height
-
-        args
-
-        width(int): width of the rectangle
-        height(int): height of the rectangle     
-        """
+        """instantiation"""
         if self.integer_validator("width", width) is True:
             self.__width = width
         if self.integer_validator("height", height) is True:

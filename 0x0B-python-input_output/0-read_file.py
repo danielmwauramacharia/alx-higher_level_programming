28713b9141cce68845da196481f46f160df416ea
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-26 21:20:58 +0300

    adjust the print in the function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """A function that displays a passed file in the stdout"""
     with open(filename, encoding="utf-8") as fileObj:
-        print(fileObj.read())
+        print(fileObj.read(), end="")

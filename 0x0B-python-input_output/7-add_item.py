f1ce0b0f2c63332664114e3be317124e2ceedeff
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-27 22:47:01 +0300

    fix the pep-8 coding style

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,13 +4,14 @@ Module that adds all arguments to a Python list,
 
 and then save them to a file
 """
+import json
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
 try:
     argList = load_from_json_file(filename)
-except:
+except json.JSONDecodeError:
     argList = []
 if len(sys.argv) > 1:
     sysList = sys.argv[1:]

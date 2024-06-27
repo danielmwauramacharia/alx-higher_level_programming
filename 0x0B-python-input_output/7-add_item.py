cdda82c4363b135f783bc024d91c37b5b331f587
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-27 21:41:35 +0300

    adjust the code

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """ Module that adds all arguments to a Python list, and then save them to a file"""
 import sys
+import os
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
-argList = []
+if os.path.exists(filename):
+    argList = load_from_json_file(filename)
+else:
+    argList = []
 if len(sys.argv) > 1:
     sysList = sys.argv[1:]
     for item in sysList:
         argList.append(sysList)
 save_to_json_file(argList, filename)
-load_from_json_file(filename)

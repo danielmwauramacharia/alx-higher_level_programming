7c1c349003dae81b037d1a7744e7487941fcf20b
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-27 23:31:41 +0300

    module documentation final

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list,and then save them to a file"""
+""" 
+Module that adds all arguments to a Python list,
+
+and then save them to a file
+"""
 
 import json
 import sys

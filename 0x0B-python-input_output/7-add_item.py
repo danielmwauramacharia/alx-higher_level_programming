f56ecfb484bd7b84890566881d800aa438bdab82
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-27 15:45:24 +0300

    adding items to a JSON file and the loading data from there

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module that adds all arguments to a Python list, and then save them to a file"""
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+filename = "add_item.json"
+argList = []
+if len(sys.argv) > 1:
+    sysList = sys.argv[1:]
+    for item in sysList:
+        argList.append(sysList)
+save_to_json_file(argList, filename)
+load_from_json_file(filename)

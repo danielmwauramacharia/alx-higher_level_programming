867fe0c6baf91a1ff369234156a3540c853df0fb
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-27 13:58:21 +0300

    changes on the file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,13 +5,7 @@ import json
 
 def load_from_json_file(filename):
     """Function that creates an Object from a JSON file"""
-    with open(filename, mode="w", encoding="utf-8") as f:
-        return json.load(f)
-    #     pyObject = json.loads(f)
-    # return pyObject
-
-
-filename = "my_list.json"
-my_list = load_from_json_file(filename)
-print(my_list)
-print(type(my_list))
+    with open(filename, encoding="utf-8") as f:
+        rd = f.read()
+        json.load(rd)
+    return rd

a2866442d2e20b6b99a0f77c5eca2d006f15a7d0
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-06-28 15:09:40 +0300

    pystyle fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,28 +2,9 @@
 """
 Module: add_item
 
-This module provides functionality to add command-line arguments to a list
-stored in a JSON file. It allows users to accumulate a list of items incrementally
-through multiple invocations of the script. The list is saved to and loaded from
-a JSON file named 'add_item.json'.
-
-Usage:
-    ./add_item.py item1 item2 item3
-
-Dependencies:
-    - Python 3.x
-    - json module (standard library)
-    - sys module (standard library)
-    - 6-load_from_json_file.py: Provides the function load_from_json_file
-    - 5-save_to_json_file.py: Provides the function save_to_json_file
-
-Functions:
-    load_from_json_file(filename): Loads and returns a list from a JSON file.
-    save_to_json_file(my_list, filename): Saves a list to a JSON file.
-
-Error Handling:
-    The script handles JSON decoding errors by initializing an empty list if the
-    JSON file is empty or does not exist.
+add command-line arguments to a list
+stored in a JSON file.
+The list is saved to and loaded from a JSON file named 'add_item.json'.
 """
 
 import json

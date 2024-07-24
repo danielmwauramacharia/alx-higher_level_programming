83bb3f5bf3ae3550b61c75b290ee563ddd35b6da
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-07-24 16:37:12 +0300

    adding the base class that keep track of the objects

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+"""The base module"""
+
+
+class Base():
+    """Class to keep track of other classes and objects"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialization of the id attribute if defined"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects

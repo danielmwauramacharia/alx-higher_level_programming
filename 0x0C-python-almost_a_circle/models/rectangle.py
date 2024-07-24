027fdd763701334c88a12cd59885b26bd7b0eda3
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-07-24 19:02:28 +0300

    deal with import module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Rectangle module"""
-Base = __import__('base').Base
+"""Rectangle module and inherits from Base class"""
+from models.base import Base
 
 
 class Rectangle(Base):

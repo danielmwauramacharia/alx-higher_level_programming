243f0b9131e798b913ce794e7a4bb997120e35b0
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-07-29 16:51:58 +0300

    using dumps() function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """The base module"""
+import json
 
 
 class Base():
@@ -18,5 +19,5 @@ class Base():
     def to_json_string(list_dictionaries):
         """Return JSON string representation of list_dictionaries"""
         if list_dictionaries:
-            return list_dictionaries
+            return json.dumps(list_dictionaries)
         return []
